b20283ad6dbe49ac3ea2718458e3406664c138b0
Rabiagul barug <rabiagulbarug@Rabiagul-MacBook-Pro.local>
2022-07-31 20:24:43 +0300

    fix: stats defensive

--- src/pages/api/stats/index.js
+++ src/pages/api/stats/index.js
@@ -2,14 +2,14 @@ import orders from '../../../assets/database/orders.json';
 import users from '../../../assets/database/users.json';
 
 export default async function handler(req, res) {
-  const totalAmount = orders.filter(order => order.status === 'completed').reduce((acc, order) => {
-    return acc + Number(order.totalPrice);
-  }, 0);
-  const totalUsers = users.length;
-  const totalOrders = orders.length;
-  const newOrders = orders.filter(order => order.status === 'pending').length;
-  const enRouteOrders = orders.filter(order => order.status === 'enRoute').length;
-  const completedOrders = orders.filter(order => order.status === 'completed').length;
+  const totalAmount = orders?.filter(order => order?.status === 'completed').reduce((acc, order) => {
+    return acc + Number(order?.totalPrice ?? 0);
+  }, 0) ?? 0;
+  const totalUsers = users?.length ?? 0;
+  const totalOrders = orders?.length ?? 0;
+  const newOrders = orders?.filter(order => order?.status === 'pending')?.length ?? 0;
+  const enRouteOrders = orders?.filter(order => order?.status === 'enRoute')?.length ?? 0;
+  const completedOrders = orders?.filter(order => order?.status === 'completed')?.length ?? 0;
 
   res.json({
     data: {
