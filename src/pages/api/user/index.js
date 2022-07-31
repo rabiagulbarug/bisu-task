445a39fb2bef07eb89ddd22579289d45596955b0
Rabiagul barug <rabiagulbarug@Rabiagul-MacBook-Pro.local>
2022-07-31 20:29:44 +0300

    fix: user index api

--- src/pages/api/user/index.js
+++ src/pages/api/user/index.js
@@ -4,18 +4,18 @@ export default async function handler(req, res) {
   const { role } = req.query;
   switch (role) {
     case 'admin':
-      res.json({data: users.filter(user => user.role === 'admin')}).status(200);
+      res.json({data: users.filter(user => user.role === 'admin')});
       break;
     case 'support':
-      res.json({data: users.filter(user => user.role === 'support')}).status(200);
+      res.json({data: users.filter(user => user.role === 'support')});
       break;
     case 'customer':
-      res.json({data: users.filter(user => user.role === 'customer')}).status(200);
+      res.json({data: users.filter(user => user.role === 'customer')});
       break;
     case 'all':
     default:
-      res.json({data: users}).status(200)
+      res.json({data: users});
       break;
   }
-  res.json({message: 'not found'}).status(404);
+  res.json({message: 'not found', error: true});
 }
