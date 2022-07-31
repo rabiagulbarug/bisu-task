a26a9aecbef4429970b4c8c05aea6104c78ea690
Rabiagul barug <rabiagulbarug@Rabiagul-MacBook-Pro.local>
2022-07-31 20:29:03 +0300

    fix: auth login api

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -3,12 +3,12 @@ import users from '../../../assets/database/users.json';
 export default async function handler(req, res) {
   const {email, password} = JSON.parse(req.body);
   if (req.method !== 'POST') {
-    res.status(405).json({message: 'method not allowed'});
+    res.json({message: 'method not allowed', error: true});
   }
   const user = users.find(t => t.email === email && t.password === password);
   if (!user) {
-    res.status(400).json({message: 'invalid credentials'});
+    res.json({message: 'invalid credentials', error: true});
   }
 
-  return res.status(200).json({data: user});
+  return res.json({data: user});
 }
